e5b5adef6783764b1872618cc39086f70bb7e1f8
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2019-05-15 13:02:26 +0100

    linter which warns when a step is too long

--- testing/cucumber/step_definitions/setup_steps.rb
+++ testing/cucumber/step_definitions/setup_steps.rb
@@ -41,8 +41,16 @@ Given(/^a linter for tests with too many steps$/) do
   @linter = CukeLinter::TestWithTooManyStepsLinter.new
 end
 
+Given("a linter for test steps with too many characters") do
+  @linter = CukeLinter::TestStepWithTooManyCharactersLinter.new
+end
+
 Given(/^a linter for tests with too many steps has been registered$/) do
   CukeLinter.register_linter(linter: CukeLinter::TestWithTooManyStepsLinter.new, name: 'TestWithTooManyStepsLinter')
+end
+
+Given(/^a linter for test steps with too many characters has been registered$/) do
+  CukeLinter.register_linter(linter: CukeLinter::TestStepWithTooManyCharactersLinter.new, name: 'TestStepWithTooManyCharactersLinter')
 end
 
 Given(/^the following configuration file(?: "([^"]*)")?:$/) do |file_name, text|
