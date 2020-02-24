ea2d1fb6822d5c454251894c41812dc4d6555a6b
Thiago Trentin <thiagotr@compasso.com.br>
2020-02-24 13:54:20 -0300

    - add keywords setup as language support

--- lib/cuke_linter/linters/test_with_setup_step_as_final_step_linter.rb
+++ lib/cuke_linter/linters/test_with_setup_step_as_final_step_linter.rb
@@ -4,6 +4,11 @@ module CukeLinter
 
   class TestWithSetupStepAsFinalStepLinter < Linter
 
+    # Changes the linting settings on the linter using the provided configuration
+    def configure(options)
+      DialectHelper.set_given_keywords(options)
+    end
+
     # The rule used to determine if a model has a problem
     def rule(model)
       return false unless model.is_a?(CukeModeler::Scenario) || model.is_a?(CukeModeler::Outline)
@@ -11,7 +16,7 @@ module CukeLinter
       model_steps = model.steps || []
       return false unless model_steps.last
 
-      model_steps.last.keyword == 'Given'
+      DialectHelper.get_given_keywords.include?(model_steps.last.keyword)
     end
 
     # The message used to describe the problem that has been found
