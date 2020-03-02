6269eaa69a8fe2881d453b1d9525bafa50b383c9
Eric Kessler <morrow748@gmail.com>
2020-03-01 18:43:35 -0800

    Remove unneeded files
    Removing the dialect module because it is no longer used.

--- lib/cuke_linter/helpers/dialect_helper.rb
+++
@@ -1,7 +0,0 @@
-module CukeLinter
-  module DialectHelper
-    def self.get_configured_keywords(options, keyword)
-      options[keyword] if options[keyword]&.instance_of?(Array)
-    end
-  end
-end
