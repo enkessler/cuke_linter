ec519fc78c6b5ae76f7f78529da5f9872b208613
Eric Kessler <morrow748@gmail.com>
2020-04-23 23:36:07 -0700

    Clean up some code
    Simplifying some boolean logic.

--- lib/cuke_linter/linters/test_with_bad_name_linter.rb
+++ lib/cuke_linter/linters/test_with_bad_name_linter.rb
@@ -9,10 +9,10 @@ module CukeLinter
       return false unless model.is_a?(CukeModeler::Scenario) || model.is_a?(CukeModeler::Outline)
 
       lowercase_name = model.name.downcase
-      return true if lowercase_name.include?('test') ||
-                     lowercase_name.include?('verif') ||
-                     lowercase_name.include?('check')
-      false
+
+      lowercase_name.include?('test') ||
+          lowercase_name.include?('verif') ||
+          lowercase_name.include?('check')
     end
 
     # The message used to describe the problem that has been found
