9f7c7ef290e7eddca326716e77985bd0ca52c5df
Eric Kessler <morrow748@gmail.com>
2020-04-25 11:49:10 -0700

    Clean up code
    Improving the readability of the pretty formatter by breaking it down
    into smaller methods instead of it being just one big one.

--- lib/cuke_linter/formatters/pretty_formatter.rb
+++ lib/cuke_linter/formatters/pretty_formatter.rb
@@ -1,55 +1,65 @@
 module CukeLinter
 
   # Formats linting data into organized, user readable text
-
   class PrettyFormatter
 
     # Formats the given linting data
     def format(data)
-      categorized_problems = Hash.new { |linters, linter_name| linters[linter_name] = Hash.new { |problems, problem| problems[problem] = [] } }
+      format_data(categorize_problems(data), data.count)
+    end
 
-      data.each do |lint_item|
-        categorized_problems[lint_item[:linter]][lint_item[:problem]] << lint_item[:location]
-      end
 
-      formatted_data = ''
+    private
 
-      categorized_problems.each_pair do |linter, problems|
-        formatted_data << linter + "\n"
 
-        problems.each_pair do |problem, locations|
-          formatted_data << "  #{problem}" + "\n"
-
-          sorted_locations = locations.sort do |a, b|
-            file_name_1   = a.match(/(.*?)(?::\d+)?$/)[1]
-            line_number_1 = a.match(/:(\d+)$/) ? a.match(/:(\d+)$/)[1].to_i : 0
-            file_name_2   = b.match(/(.*?)(?::\d+)?$/)[1]
-            line_number_2 = b.match(/:(\d+)$/) ? b.match(/:(\d+)$/)[1].to_i : 0
-
-            case
-              when (file_name_1 < file_name_2) ||
-                  (file_name_1 == file_name_2) && (line_number_1 < line_number_2)
-                -1
-              when (file_name_1 > file_name_2) ||
-                  (file_name_1 == file_name_2) && (line_number_1 > line_number_2)
-                1
-              else
-                0
-            end
-          end
-
-          sorted_locations.each do |location|
-            formatted_data << "    #{location}\n"
-          end
+    def categorize_problems(data)
+      {}.tap do |categorized_problems|
+        data.each do |lint_item|
+          categorized_problems[lint_item[:linter]]                      ||= {}
+          categorized_problems[lint_item[:linter]][lint_item[:problem]] ||= []
+          categorized_problems[lint_item[:linter]][lint_item[:problem]] << lint_item[:location]
         end
       end
+    end
 
-      total_problems = data.count
-      formatted_data << "\n" unless total_problems.zero?
+    def format_data(problem_data, problem_count)
+      ''.tap do |formatted_data|
+        problem_data.each_pair do |linter, problems|
+          formatted_data << "#{linter}\n"
 
-      formatted_data << "#{total_problems} issues found"
+          problems.each_pair do |problem, locations|
+            formatted_data << "  #{problem}\n"
 
-      formatted_data
+            sort_locations(locations).each do |location|
+              formatted_data << "    #{location}\n"
+            end
+          end
+        end
+
+        formatted_data << "\n" unless problem_count.zero?
+        formatted_data << "#{problem_count} issues found"
+      end
+    end
+
+    def sort_locations(locations)
+      locations.sort do |a, b|
+        file_name_1   = a.match(/(.*?)(?::\d+)?$/)[1]
+        line_number_1 = a =~ /:\d+$/ ? a.match(/:(\d+)$/)[1].to_i : 0
+        file_name_2   = b.match(/(.*?)(?::\d+)?$/)[1]
+        line_number_2 = b =~ /:\d+$/ ? b.match(/:(\d+)$/)[1].to_i : 0
+
+        compare_locations(file_name_1, file_name_2, line_number_1, line_number_2)
+      end
+    end
+
+    def compare_locations(file_name_1, file_name_2, line_number_1, line_number_2)
+      if (file_name_1 < file_name_2) || (file_name_1 == file_name_2) && (line_number_1 < line_number_2)
+        -1
+      elsif (file_name_1 > file_name_2) || (file_name_1 == file_name_2) && (line_number_1 > line_number_2)
+        1
+      else
+        0
+      end
     end
 
   end
