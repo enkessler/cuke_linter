ae1931d9d287d25af042e7c32ea5c64e9f1924f2
Eric Kessler <morrow748@gmail.com>
2025-03-02 16:18:04 -0500

    Fix RuboCop violations
    Make RuboCop happy again.

--- lib/cuke_linter/formatters/pylint_formatter.rb
+++ lib/cuke_linter/formatters/pylint_formatter.rb
@@ -1,20 +1,22 @@
 module CukeLinter
+
   # Formats linting data into the PyLint format
   # path/to/file:line:col: [Rule] Message
   class PylintFormatter
+
     # Formats the given linting data
     def format(data)
       ''.tap do |formatted_data|
         data.each do |problem|
-          location = problem[:location].split(":")
+          location = problem[:location].split(':')
           # Linters at the very least don't specify column numbers, but for feature file wide errors
           # there's no line numbers either. Pad the location out if we need to.
-          while location.length() < 3
-            location.insert(-1, "")
-          end
-          formatted_data << "#{location.join(separator=':')}: [#{problem[:linter]}] #{problem[:problem]}\n"
+          location.insert(-1, '') while location.length < 3
+          formatted_data << "#{location.join(':')}: [#{problem[:linter]}] #{problem[:problem]}\n"
         end
       end
     end
+
   end
+
 end
